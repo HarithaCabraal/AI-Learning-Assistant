5d84e53210671b5e0126bf9bc27eeed9e02077fc
Haritha Cabraal <hdcabraal@gmail.com>
2026-05-14 16:02:32 +0530

    created the apiPaths

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,8 @@ import documentRoutes from './routes/documentRoutes.js';
 import flashcardRoutes from './routes/flashcardRoutes.js';
 import aiRoutes from './routes/aiRoutes.js';
 import quizRoutes from './routes/quizRoutes.js';
+import progressRoutes from './routes/progressRoutes.js';
+
 
 
 //ES6 module __dirname alternative
@@ -46,6 +48,7 @@ app.use('./api/documents', documentRoutes);
 app.use('/api/flashcards', flashcardRoutes);
 app.use('/api/ai', aiRoutes);
 app.use('/api/quizzes', quizRoutes);
+app.use('/api/progress', progressRoutes);
 
 
 app.use(errorHandler);
